3388903c0811e95279d7635a66e08c6538a2b341
Rafael Gonçalves Ribeiro <raffafullstack@gmail.com>
2024-03-18 16:05:25 -0300

    fazendo flip card através do .js

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,16 +1,17 @@
 $(document).ready(function(){
 
-    // Capta um click na imagem de perfil que possui .imgProfile.imgProfile1, aplica a classe hidden nela e remove a classe hidden do qr code
-    // Se a imagem de perfil tiver a classe hidden, remove dela e aplica no qr code que possui .imgProfile.imgProfile2
+    // Capta um click na imagem ".imgProfile" ativa, aplica a classe hidden nessa e remove a classe hidden da ".imgProfile" inativa
     $('.imgProfile').click(function(){
-        const foto = $('.imgProfile1')
-        const codigo = $('.imgProfile2')
-        foto.toggleClass('hidden');
-        codigo.toggleClass('hidden');
+        const imgChange = $('.imgProfile')
+        imgChange.toggleClass('flip');
+
+        setTimeout(function(){
+            imgChange.toggleClass('hidden');
+        }, 350)
     });
 
     // Com window eu seleciono o objeto (no caso a janela do navegador)
-    // .bind vincula a função anonima (já que não possui trigger como .hover ou .click, por exemplo) com o manipulador de evento 'resize
+    // .bind vincula a função anonima (já que não possui trigger como .hover ou .click, por exemplo) com o manipulador de evento 'resize'
     // A função em si é uma verificação que aplicará ou removerá uma classe em body de acordo com o resultado.
     $(window).bind('resize', function() {
         if (screen.height < screen.width) {
@@ -20,5 +21,4 @@ $(document).ready(function(){
         }
     });
 });
-
-    // Dentro do arquivo .scss nós teremos a classe "virado" com quatro linhas de código para a mesma finalidade, cada uma para um caso
+    // Dentro do arquivo .scss nós utiizaremos a classe .virado visando a responsividade apenas para o caso da rotação no mobile
